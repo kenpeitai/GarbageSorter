be7f724098f4ceca92b51a0dc1f3a3953dc25d4d
kenpeitai <2443595035@qq.com>
2023-04-24 23:01:59 +0800

    1.0 开发中 4/24 总进度70%,新增收藏，下拉栏，布局待调整，下拉栏功能待实现

--- app/src/main/java/com/cqupt/garbagesorter/fragment/FragmentThree.kt
+++ app/src/main/java/com/cqupt/garbagesorter/fragment/FragmentThree.kt
@@ -1,35 +1,44 @@
 package com.cqupt.garbagesorter.fragment
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.cqupt.garbagesorter.R
+import com.cqupt.garbagesorter.activity.GarbageInfoActivity
 import com.cqupt.garbagesorter.db.MyDatabase
 import com.cqupt.garbagesorter.db.bean.Garbage
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 // TODO: Rename parameter arguments, choose names that match
@@ -75,71 +84,164 @@ class FragmentThree : Fragment() {
         return view
     }
 
+
+    @OptIn(ExperimentalMaterialApi::class)
     @Composable
     private fun SetComposeView() {
         val datebase = MyDatabase.getDatabase(LocalContext.current)
-        val garbages = remember { mutableStateListOf<Garbage>() }
-        LaunchedEffect(key1 = garbages, block = {
+        var garbages = remember { mutableStateListOf<Garbage>() }
+        var count by remember { mutableStateOf(0) }
+        var showProgress by remember { mutableStateOf(false) }
+
+        LaunchedEffect(key1 = garbages, key2 = count, block = {
             withContext(Dispatchers.IO) {
                 val result = datebase.GarbageDao()?.getCollection(1)
                 garbages.clear()
                 if (result != null) {
                     garbages.addAll(result)
                 }
+                delay(500)
+                showProgress = false
             }
         })
 
         Column(modifier = Modifier.padding(15.dp)) {
             Row(
                 modifier = Modifier
-                    .height(80.dp)
+                    .height(0.dp)
                     .fillMaxWidth()
                     .background(color = Color.DarkGray)
             ) {
             }
-            LazyColumn() {
-                items(garbages.size) { index: Int ->
-                    Row(
-                        modifier = Modifier
-                            .height(200.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Column(modifier = Modifier.weight(1f)) {
-                            val name = "a" + garbages[index].id + "_1"
-                            val drawableId = requireContext().resources.getIdentifier(
-                                name,
-                                "drawable",
-                                requireContext().packageName
-                            )
-                            Image(
-                                painter = painterResource(id = drawableId),
-                                contentDescription = "",
-                                modifier = Modifier.align(Alignment.CenterHorizontally)
-                            )
-                            garbages[index].name?.let {
-                                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){ Text(
-                                    text = it, modifier = Modifier
-                                        .fillMaxSize()
-                                        .align(Alignment.Center),
-                                )}
+            val refreshState = rememberPullRefreshState(refreshing = false, onRefresh = {
+                count++
+                Toast.makeText(requireContext(), "刷新中 count:$count", Toast.LENGTH_SHORT).show()
+                showProgress = true
+            })
+            Box(modifier = Modifier.pullRefresh(state = refreshState, enabled = true)) {
+                LazyColumn() {
+                    items(garbages.size) { index: Int ->
+                        var expanded by remember { mutableStateOf(false) }
+                        var selectedIndex by remember { mutableStateOf(0) }
+                        val items = listOf("Option 1", "Option 2", "Option 3")
 
-                            }
-                        }
-                        Column(modifier = Modifier.weight(1f)) {
-                            Row(horizontalArrangement = Arrangement.End,modifier = Modifier.padding(5.dp)) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.baseline_expand_more_24),
-                                    contentDescription ="",
+                        Row(
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .fillMaxWidth()
+                                .shadow(
+                                    elevation = 4.dp,
+                                    shape = RoundedCornerShape(8.dp),
+                                    clip = true
                                 )
-                                garbages[index].type?.let { Text(text = it, modifier = Modifier
-                                    .fillMaxWidth()
-                                    .align(Alignment.CenterVertically)) }
+                                .clip(RoundedCornerShape(6.dp))
+                                .background(Color.White)
+                                .clickable {
+                                    val intent = Intent(context, GarbageInfoActivity::class.java)
+                                    intent.putExtra("EXTRA_GARBAGE", garbages[index].id)
+                                    startActivity(intent)
+                                }
+                        ) {
+                            Column(modifier = Modifier.weight(1f)) {
+                                val name = "a" + garbages[index].id + "_1"
+                                val drawableId = requireContext().resources.getIdentifier(
+                                    name,
+                                    "drawable",
+                                    requireContext().packageName
+                                )
+                                Image(
+                                    painter = painterResource(id = drawableId),
+                                    contentDescription = "",
+                                    modifier = Modifier
+                                        .padding(horizontal = 5.dp, vertical = 8.dp)
+                                        .align(Alignment.CenterHorizontally)
+                                        .clip(shape = RoundedCornerShape(8.dp))
+                                )
+                                garbages[index].name?.let {
+                                    Box(
+                                        modifier = Modifier.fillMaxSize(),
+                                        contentAlignment = Alignment.Center
+                                    ) {
+                                        Text(
+                                            text = it,
+                                            modifier = Modifier
+                                                .align(Alignment.Center)
+                                                .padding(bottom = 10.dp),
+                                            style = MaterialTheme.typography.subtitle1
+                                        )
+                                    }
+
+                                }
                             }
-                            garbages[index].description?.let { Text(text = it, modifier = Modifier
-                                .padding(5.dp)
-                                .align(Alignment.CenterHorizontally)) }
+                            Column(modifier = Modifier.weight(1.5f)) {
+                                Row(modifier = Modifier.padding(5.dp)) {
+                                    Icon(
+                                        painter = painterResource(id = R.drawable.baseline_expand_more_24),
+                                        contentDescription = "",
+                                        modifier = Modifier.clickable { expanded = true }
+                                    )
+
+                                    DropdownMenu(
+                                        expanded = expanded,
+                                        onDismissRequest = { expanded = false },
+                                        modifier = Modifier.wrapContentSize()
+                                    ) {
+                                        items.forEachIndexed { index, item ->
+                                            Column(modifier = Modifier.fillMaxWidth().padding(vertical = 0.dp)) {
+                                                DropdownMenuItem(modifier = Modifier.height(40.dp).padding(vertical=0.dp), onClick = {
+                                                    selectedIndex = index
+                                                    expanded = false
+                                                }) {
+                                                    Text(text = item)
+                                                }
+                                                Divider(
+                                                    color = Color.Gray,
+                                                    thickness = 0.5.dp,
+                                                    modifier = Modifier.fillMaxWidth()
+                                                )
+
+                                            }
+
+                                        }
+
+
+                                    }
+                                    garbages[index].type?.let {
+                                        Text(
+                                            text = it,
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .align(Alignment.CenterVertically),
+                                            style = TextStyle(
+                                                fontStyle = FontStyle.Italic,
+                                                fontWeight = FontWeight.Light
+                                            )
+                                        )
+                                    }
+                                    Image(
+                                        painter = painterResource(id = R.drawable.ic_other),
+                                        contentDescription = "img_type"
+                                    )
+                                }
+                                garbages[index].description?.let {
+                                    Text(
+                                        text = it, modifier = Modifier
+                                            .padding(5.dp)
+                                            .align(Alignment.CenterHorizontally)
+                                    )
+                                }
+                            }
                         }
+                        Spacer(modifier = Modifier.height(10.dp))
                     }
+                }
+                PullRefreshIndicator(
+                    refreshing = false,
+                    state = refreshState,
+                    modifier = Modifier.align(Alignment.TopCenter)
+                )
+                if (showProgress) {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
                 }
             }
         }
